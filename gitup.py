52577a3153e4e0caa56cb82c2724ad419d101d9c
Your Name <you@example.com>
2019-03-08 19:55:33 +0100

    v0.4: multi-threaded fetching

--- gitup.py
+++ gitup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 # coding: utf-8
 #
-# version: 0.2@20180517
+# version: 0.2 @ 20180517
 #   [X] Python2 to Python3
 #   [X] Using glob instead of os.walk
 #   [X] List all available updates and ask for confirmation
@@ -10,6 +10,14 @@
 #   [X] Add requirements.txt for easy dependances installation via pip
 #   [X] Change informative output
 #   [X] Checking `git status` before updating
+#
+#
+# version: 0.4 @ 20190308
+#   [X] Printing the path of the repository when fetching
+#   [ ] Multi-threading the fetching
+#
+# version: x.x
+#   [ ] Logs all in a file
 #
 HEADER = """  ___  ____  ____    __  __  ____  ____    __   ____  ____ 
  / __)(_  _)(_  _)  (  )(  )(  _ \(  _ \  /__\ (_  _)( ___)
@@ -22,8 +30,44 @@ from termcolor import colored
 import os, sys, argparse, time, subprocess
 from glob import glob
 from pathlib import Path
+from threading import Thread
 
 GIT_PATH = '/usr/bin/git'
+
+repositories_to_update = []
+global repositories_to_update
+
+def fetch(path_to_repo):
+    if path_to_repo.find('/') > -1:
+        repository_name = path_to_repo.split('/')[-1]
+    else:
+        repository_name = path_to_repo
+    print(colored("[-] Fetch git repository", 'green'), colored("%s" % repository_name, 'green', attrs=['bold']), colored("(%s)" % path_to_repo, 'green'))
+
+    absolute_path = str(Path(path_to_repo).resolve())
+
+    # `git fetch` before, otherwise we will have wrong results from `git status` 
+    git_process = subprocess.Popen(
+            [GIT_PATH, 'fetch'],
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            cwd=absolute_path)
+    git_process.wait()
+    git_process = subprocess.Popen(
+        [GIT_PATH, 'status', '-uno', '.'],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        cwd=absolute_path)
+    
+    stdout = git_process.communicate()[0].decode()
+    
+    if not "Your branch is up-to-date" in stdout:
+        repositories_to_update.append({
+            'name': repository_name,
+            'absolute_path': absolute_path
+        })
+    
+    return
 
 if __name__ == '__main__':
     print(colored(HEADER, 'magenta'))
@@ -52,13 +96,21 @@ if __name__ == '__main__':
         current_dir = t.replace('/.git/HEAD', '')
         git_repositories.add(current_dir)
 
-    repositories_to_update = []
+    #repositories_to_update = []
+    threads = []
     for path_to_repo in git_repositories:
+        threads.append(Thread(target=fetch, args=(path_to_repo,)))
+        threads[-1].start()
+    
+    for t in threads:
+        t.join()
+
+        """
         if path_to_repo.find('/') > -1:
             repository_name = path_to_repo.split('/')[-1]
         else:
             repository_name = path_to_repo
-        print(colored("[-] Fetch git repository", 'green'), colored("%s" % repository_name, 'green', attrs=['bold']))
+        print(colored("[-] Fetch git repository", 'green'), colored("%s" % repository_name, 'green', attrs=['bold']), colored("(%s)" % path_to_repo, 'green'))
 
         absolute_path = str(Path(path_to_repo).resolve())
 
@@ -82,6 +134,7 @@ if __name__ == '__main__':
                 'name': repository_name,
                 'absolute_path': absolute_path
             })
+        """
     
     if len(repositories_to_update) == 0:
         print(colored("[-] All repositories (%i) are up-to-date" % len(git_repositories), 'blue'))
